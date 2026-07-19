4aad2e55b1a85e0e8f1619956a2fcab84e072907
dohoangminhdohoangminh1-svg <dohoangminhdohoangminh1@gmail.com>
2026-07-20 02:47:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Này, baby biết không! baby là người đáng yêu nhất mà a từng gặp! 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
